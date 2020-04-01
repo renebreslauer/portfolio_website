b9ece5e7aeebc8ec03ef8962a198a8e6a9c50c5d
renebreslauer <renebreslauer@me.com>
2020-04-01 19:18:59 -0400

    navbar modifications

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,14 @@
 import React from 'react'
 import '../App.css'
-import 'font-awesome/css/font-awesome.min.css';
 
 function Footer() {
   return (
     <div className="footer">
-        <div className="footer_content">
-          <i class="fab fa-github"></i>
-          <i class="fab fa-linkedin"></i>
-          <p>hello</p>
-
-
-          
-        </div>
+      <div className="footer_content">
+        <i class="fab fa-github"></i>
+        <i class="fab fa-linkedin"></i>
+      </div>
     </div>
-     
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,71 +1,64 @@
 import React from 'react'
 import '../App.css'
-import HomeIcon from '../components/assets/Home_icon.svg'
+import Final_R from '../components/assets/final_r.svg'
 import AboutIcon from '../components/assets/About_icon.svg'
 import SkillsIcon from '../components/assets/Skills_icon.svg'
 import ContactIcon from '../components/assets/Contact_icon.svg'
+import PortfolioIcon from '../components/assets/Portfolio_icon.svg'
 import { NavLink } from 'react-router-dom'
 
 function MyNavbar() {
   return (
     <div className="nav_wrapper">
-      <div className="nav_container">
-        <div className="nav_div" id="home_div">
-          <NavLink
-            to="/"
-            className="nav_link"
-            id="home_link"
-            label="home"
-            activeClassName="nav_link--active"
-            to="/"
-          >
-            <p className="link_text">Home</p>
-            <img src={HomeIcon} className="nav_icon" />
-          </NavLink>
-        </div>
-        <div className="nav_div" id="about_div">
-          <NavLink
-            to="/About"
-            className="nav_link"
-            id="about_link"
-            activeClassName="nav_link--active"
-          >
-            <p className="link_text">About</p>
-            <img src={AboutIcon} className="nav_icon" />
-          </NavLink>
-        </div>
-        <div className="nav_div" id="skills_div">
-          <NavLink
-            to="/Skills"
-            className="nav_link"
-            id="skills_link"
-            activeClassName="nav_link--active"
-          >
-            <p className="link_text">Skills</p>
-            <img src={SkillsIcon} className="nav_icon" />
-          </NavLink>
-        </div>
-        <div className="nav_div" id="portfolio_div">
-          <NavLink
-            to="/Portfolio"
-            className="nav_link"
-            id="portfolio_link"
-            activeClassName="nav_link--active"
-          >
-            <p className="link_text">Portfolio</p>
-          </NavLink>
-        </div>
-        <div className="nav_div" id="contact_div">
-          <NavLink
-            to="/Contact"
-            className="nav_link"
-            id="contact_link"
-            activeClassName="nav_link--active"
-          >
-            <p className="link_text">Contact</p>
-            <img src={ContactIcon} className="nav_icon" />
-          </NavLink>
-        </div>
+      <div className="nav-logo">
+        <NavLink to="/" id="home_link" label="home" to="/">
+          <img src={Final_R} className="logo-icon" />
+        </NavLink>
+      </div>
+      <div className="nav-body"></div>
+      <div className="nav_div" id="about_div">
+        <NavLink
+          to="/About"
+          className="nav_link"
+          id="about_link"
+          activeClassName="nav_link--active"
+        >
+          <p className="link_text">About</p>
+          <img src={AboutIcon} className="nav_icon" />
+        </NavLink>
+      </div>
+      <div className="nav_div" id="skills_div">
+        <NavLink
+          to="/Skills"
+          className="nav_link"
+          id="skills_link"
+          activeClassName="nav_link--active"
+        >
+          <p className="link_text">Skills</p>
+          <img src={SkillsIcon} className="nav_icon" />
+        </NavLink>
+      </div>
+      <div className="nav_div" id="portfolio_div">
+        <NavLink
+          to="/Portfolio"
+          className="nav_link"
+          id="portfolio_link"
+          activeClassName="nav_link--active"
+        >
+          <p className="link_text">Portfolio</p>
+          <img src={PortfolioIcon} className="nav_icon" />
+        </NavLink>
+      </div>
+      <div className="nav_div" id="contact_div">
+        <NavLink
+          to="/Contact"
+          className="nav_link"
+          id="contact_link"
+          activeClassName="nav_link--active"
+        >
+          <p className="link_text">Contact</p>
+          <img src={ContactIcon} className="nav_icon" />
+        </NavLink>
       </div>
     </div>
   )

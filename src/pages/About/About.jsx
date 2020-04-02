74be38942724fd23e35ef9dbced0c764879736d5
renebreslauer <renebreslauer@me.com>
2020-04-02 01:11:00 -0400

    about page content

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,13 +1,44 @@
 import React from 'react'
-import '../../App.css'
+import styles from './About.module.css'
+import Placeholder from '../../components/assets/placeholder_image.png'
 
 function About() {
   return (
-   
-      <div className="About_content">
-        <h2>About Me</h2>
+    <>
+      <div className={styles.about_wrapper}>
+        <div className={styles.hidden_images}></div>
+
+        <div className={styles.about_content}>
+          <div className={styles.typewriter}>
+            <h2>Once Upon a Time...</h2>
+          </div>
+          <p className={styles.about_body}>
+            As a Florida native, I've spent most of my life in the sunny south.
+            I studied Apparel Design at Florida State University and progressed
+            into a career of retail management. While I loved working with
+            people, I still had a desire to build and create things. Pursuing
+            front-end development has allowed me to combine both- working in a
+            collaborative environment and creating beautiful, fully functional
+            applications.
+          </p>
+          <p className={styles.about_body}>
+            These days I reside on Miami Beach with my three pet{' '}
+            <span class={styles.special_text} id={styles.rabbit_text}>
+              rabbits
+            </span>
+            , and when I'm not coding, I'm typically working on freelance design
+            projects, illustrating, or watching Tiger King.
+          </p>
+        </div>
+        <div className={styles.hidden_images}>
+          <img
+            src={Placeholder}
+            id={styles.rabbit_image}
+            className={styles.hidden_image}
+          />
+        </div>
       </div>
-   
+    </>
   )
 }
 

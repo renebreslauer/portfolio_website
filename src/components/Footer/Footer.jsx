318471cfc1e8231e22176b98c6cedea6ada98e48
renebreslauer <renebreslauer@RENEs-iMac.local>
2020-04-05 15:23:02 -0400

    footer placement

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Footer.module.css'
+import { Link } from 'react-router-dom'
+
+function Footer() {
+    return (
+    <footer>
+        <h2>footer</h2>
+    </footer>
+    )
+  }
+  
+  export default Footer
+  
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,2 @@
 export { default as Navbar } from '../components/Navbar/Navbar'
+export { default as Footer } from '../components/Footer/Footer'

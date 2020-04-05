70af28b4c5ad499cb245ae49964f79a67dbd4d30
renebreslauer <renebreslauer@RENEs-iMac.local>
2020-04-05 16:19:09 -0400

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,12 @@ function Footer() {
     return (
     <footer>
     <div className={styles.footer_wrapper}>
+        <div className={styles.footer_info}>
+            <p>For business inquiries, please email renebreslauer@me.com</p>
+            <p>Website built with React - NodeJS - ExpressJS</p>
+        </div>
         <div className={styles.links_container}>
+            <p className={styles.link_title}>Let's connect:</p>
             <p>Github</p>
             <p>Linkedin</p>
             <p>Twitter</p>

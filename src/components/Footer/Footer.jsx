1e077169f7d1421cd963c229181df3a58d7b1a15
renebreslauer <renebreslauer@RENEs-iMac.local>
2020-04-05 15:35:57 -0400

    footer formatting

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,13 @@ function Footer() {
     return (
     <footer>
     <div className={styles.footer_wrapper}>
-        <h2>footer</h2>
+        <div className={styles.links_container}>
+            <p>Github</p>
+            <p>Linkedin</p>
+            <p>Twitter</p>
+            <p>Behance</p>
+            <p>Dribbble</p>
+        </div>
     </div>
     </footer>
     )

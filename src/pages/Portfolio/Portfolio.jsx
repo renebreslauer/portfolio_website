66230c5fc208affd43a71d1b75fd8a470dbd001a
renebreslauer <renebreslauer@RENEs-iMac.local>
2020-04-05 21:11:58 -0400

    fixed nav/footer styling bugs

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
-
-import '../../App.css'
+import styles from './Portfolio.module.css'
 
 function Portfolio() {
   return (
-    <div>
-      <h2>Portfolio</h2>
+    <>
+    <div className={styles.portfolio_wrapper}>
+    <div className={styles.project_one_wrapper}>
+      <div className={styles.project_footer}>
+        <p>Live Site</p>
+        <p>Source Code</p>
+      </div>
     </div>
+    <div className={styles.project_two_wrapper}>
+      <div className={styles.project_footer}>
+      <p>Live Site</p>
+        <p>Source Code</p>
+      </div>
+    </div>
+      </div>
+    </>
   )
 }
 

5c26964779f5ab7ddf462b856feafdaccbbafc9c
renebreslauer <renebreslauer@RENEs-iMac.local>
2020-04-05 20:00:00 -0400

    footer content and links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import styles from './Footer.module.css'
-import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+library.add(fab)
 
 function Footer() {
     return (
@@ -12,11 +16,11 @@ function Footer() {
         </div>
         <div className={styles.links_container}>
             <p className={styles.link_title}>Let's connect:</p>
-            <p>Github</p>
-            <p>Linkedin</p>
-            <p>Twitter</p>
-            <p>Behance</p>
-            <p>Dribbble</p>
+            <p><a target='_blank' href='https://github.com/renebreslauer'><FontAwesomeIcon icon={['fab', 'github']} className={styles.footer_icon}/>Github</a></p>
+            <p><a target='_blank' href='https://linkedin.com/in/renebreslauer'><FontAwesomeIcon icon={['fab', 'linkedin-in']} className={styles.footer_icon}/>Linkedin</a></p>
+            <p><a target='_blank' href='#'><FontAwesomeIcon icon={['fab', 'twitter']} className={styles.footer_icon}/>Twitter</a></p>
+            <p><a target='_blank' href='#'><FontAwesomeIcon icon={['fab', 'behance']} className={styles.footer_icon}/>Behance</a></p>
+            <p><a target='_blank' href='#'><FontAwesomeIcon icon={['fab', 'dribbble']} className={styles.footer_icon}/>Dribbble</a></p>
         </div>
     </div>
     </footer>

4a63d047ee0dc1c31b5a4284bdb3f2595ce7284f
renebreslauer <renebreslauer@RENEs-iMac.local>
2020-04-07 17:52:25 -0400

    portfolio page and animation

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -26,15 +26,16 @@ const PortfolioProjects = () => {
       <div className={styles.portfolio_wrapper}>
         <div className={styles.project_one_wrapper}>
           <div className={styles.project_header}>Mission Booked</div>
+          <div className={styles.project_subtitle}>Created while at Wyncode Academy, Mission Booked is a React-Rails app with a PostgreSQL database. It is designed to help people find volunteer opportunities and charitable organizations tailored to their interests.</div>
           <div className={styles.project_body}>
-         <animated.div
+         <a target="_blank" href="https://github.com/renebreslauer/C36_better_together_now_2"><animated.div
               className={styles.project1}
               onMouseMove={({ clientX: x, clientY: y }) =>
                 setProject1({ xys: calc(x, y) })
               }
               onMouseLeave={() => setProject1({ xys: [0, 0, 1] })}
               style={{ transform: project1.xys.interpolate(trans) }}
-            ></animated.div>
+            ></animated.div></a>
           </div>
           <div className={styles.project_footer}>
             <a target="_blank" href="https://mission-booked.herokuapp.com/">
@@ -50,15 +51,17 @@ const PortfolioProjects = () => {
         </div>
         <div className={styles.project_two_wrapper}>
           <div className={styles.project_header}>FurEver</div>
+          <div className={styles.project_subtitle}>Also created at Wyncode Academy, FurEver is a petfinding app built with React and Express JS, which utilizes Petfinder's O-Auth API. FurEver is designed to help match adoptable pets with forever homes in a fun, user-friendly interface.</div>
+
           <div className={styles.project_body}>
-            <animated.div
+            <a target="_blank" href="https://github.com/renebreslauer/C36_midterm_carolina_elias_rene_tristan"><animated.div
               className={styles.project2}
               onMouseMove={({ clientX: x, clientY: y }) =>
                 setProject2({ xys: calc(x, y) })
               }
               onMouseLeave={() => setProject2({ xys: [0, 0, 1] })}
               style={{ transform: project2.xys.interpolate(trans) }}
-            ></animated.div>
+            ></animated.div></a>
           </div>
           <div className={styles.project_footer}>
             <a target="_blank" href="http://fureverapp.herokuapp.com/">

4a6d147789c36b122fb2a1cd83ad891b84753a02
renebreslauer <renebreslauer@me.com>
2020-04-07 05:32:22 -0400

    skills animation

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -6,18 +6,80 @@ function Skills() {
     <div className={styles.skills_wrapper}>
       <div className={styles.body_text}>Content</div>
       <div className={styles.skills_list}>
-        <span id={styles.html} className={styles.skills_text}>
-          HTML
-        </span>
-        <span id={styles.css} className={styles.skills_text}>
-          CSS
-        </span>
-        <span id={styles.javascript} className={styles.skills_text}>
-          JAVASCRIPT
-        </span>
-        <span id={styles.react} className={styles.skills_text}>
-          REACT
-        </span>
+        <div id={styles.html} className={styles.skills_text}>
+          <span className={styles.letter} id={styles.letter1}>
+            H
+          </span>
+          <span className={styles.letter} id={styles.letter2}>
+            T
+          </span>
+          <span className={styles.letter} id={styles.letter3}>
+            M
+          </span>
+          <span className={styles.letter} id={styles.letter4}>
+            L
+          </span>
+        </div>
+        <div id={styles.css} className={styles.skills_text}>
+          <span className={styles.letter} id={styles.letter5}>
+            C
+          </span>
+          <span className={styles.letter} id={styles.letter6}>
+            S
+          </span>
+          <span className={styles.letter} id={styles.letter7}>
+            S
+          </span>
+        </div>
+        <div id={styles.javascript} className={styles.skills_text}>
+          <span className={styles.letter} id={styles.letter8}>
+            J
+          </span>
+          <span className={styles.letter} id={styles.letter9}>
+            A
+          </span>
+          <span className={styles.letter} id={styles.letter10}>
+            V
+          </span>
+          <span className={styles.letter} id={styles.letter11}>
+            A
+          </span>
+          <span className={styles.letter} id={styles.letter12}>
+            S
+          </span>
+          <span className={styles.letter} id={styles.letter13}>
+            C
+          </span>
+          <span className={styles.letter} id={styles.letter14}>
+            R
+          </span>
+          <span className={styles.letter} id={styles.letter15}>
+            I
+          </span>
+          <span className={styles.letter} id={styles.letter16}>
+            P
+          </span>
+          <span className={styles.letter} id={styles.letter17}>
+            T
+          </span>
+        </div>
+        <div id={styles.react} className={styles.skills_text}>
+          <span className={styles.letter} id={styles.letter18}>
+            R
+          </span>
+          <span className={styles.letter} id={styles.letter19}>
+            E
+          </span>
+          <span className={styles.letter} id={styles.letter20}>
+            A
+          </span>
+          <span className={styles.letter} id={styles.letter21}>
+            C
+          </span>
+          <span className={styles.letter} id={styles.letter22}>
+            T
+          </span>
+        </div>
       </div>
     </div>
   )

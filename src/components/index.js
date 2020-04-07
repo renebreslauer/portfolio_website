ab883fa6673ae7964a086d92a8f4988584e6f6ab
renebreslauer <renebreslauer@me.com>
2020-04-07 04:14:46 -0400

    skills page

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,2 @@
 export { default as Navbar } from '../components/Navbar/Navbar'
-export { default as Modal } from '../components/Modal/Modal'
 export { default as Footer } from '../components/Footer/Footer'
--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
-import '../../App.css'
+import styles from './Skills.module.css'
 
 function Skills() {
   return (
-    <div>
-      <h2>Skills</h2>
+    <div className={styles.skills_wrapper}>
+      <div className={styles.body_text}>Content</div>
+      <div className={styles.skills_list}>
+        <span id={styles.html} className={styles.skills_text}>
+          HTML
+        </span>
+        <span id={styles.css} className={styles.skills_text}>
+          CSS
+        </span>
+        <span id={styles.javascript} className={styles.skills_text}>
+          JAVASCRIPT
+        </span>
+        <span id={styles.react} className={styles.skills_text}>
+          REACT
+        </span>
+      </div>
     </div>
   )
 }

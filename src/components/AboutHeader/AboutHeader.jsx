35e4c1051f14787600b085500ef690da1c708da6
renebreslauer <renebreslauer@me.com>
2020-05-19 18:44:35 -0400

    responsive test

---
+++ src/components/AboutHeader/AboutHeader.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import styles from './AboutHeader.module.scss'
+import styled from 'styled-components'
+
+const AboutHeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 50%;
+`
+
+const HiddenImageContainer = styled.div`
+  justify-content: end;
+`
+function AboutHeader() {
+  return (
+    <AboutHeaderWrapper>
+      <div className={styles.about_header}>
+        <span className={styles.about_letter} id={styles.about1}>
+          A
+        </span>
+        <span className={styles.about_letter} id={styles.about2}>
+          b
+        </span>
+        <span className={styles.about_letter} id={styles.about3}>
+          o
+        </span>
+        <span className={styles.about_letter} id={styles.about4}>
+          u
+        </span>
+        <span className={styles.about_letter} id={styles.about5}>
+          t
+        </span>
+      </div>
+      <HiddenImageContainer>
+        <div className={styles.hidden_image}></div>
+      </HiddenImageContainer>
+    </AboutHeaderWrapper>
+  )
+}
+
+export default AboutHeader
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,33 +1,25 @@
 import React, { useState } from 'react'
 import styles from './About.module.scss'
 import Rabbits from '../../assets/rabbits_image.jpg'
+import { AboutHeader } from '../../components/index'
+import styled from 'styled-components'
+
+const AboutWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
 
 function About() {
   const [isShown, setIsShown] = useState(false)
 
   return (
     <>
-      <div className={styles.about_wrapper}>
-        <div className={styles.hidden_images}></div>
-
+      <AboutWrapper>
+        <AboutHeader />
         <div className={styles.about_content}>
-          <div className={styles.about_header}>
-            <span className={styles.about_letter} id={styles.about1}>
-              A
-            </span>
-            <span className={styles.about_letter} id={styles.about2}>
-              b
-            </span>
-            <span className={styles.about_letter} id={styles.about3}>
-              o
-            </span>
-            <span className={styles.about_letter} id={styles.about4}>
-              u
-            </span>
-            <span className={styles.about_letter} id={styles.about5}>
-              t
-            </span>
-          </div>
           <p className={styles.about_body}>
             As a Florida native, I've spent most of my life in the sunny south.
             I studied Apparel Design at Florida State University and progressed
@@ -63,7 +55,7 @@ function About() {
             </div>
           </>
         )}
-      </div>
+      </AboutWrapper>
     </>
   )
 }

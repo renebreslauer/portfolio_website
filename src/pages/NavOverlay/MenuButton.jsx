89975681985f42514def036f515a23b1a1ca1cd5
renebreslauer <renebreslauer@me.com>
2020-05-20 00:50:13 -0400

    logo animation

--- src/pages/NavOverlay/MenuButton.jsx
+++ src/pages/NavOverlay/MenuButton.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Menu.scss'
 import Final_R from '../../assets/final_r.svg'
-
+import { RAnimationResized } from '../../components/index'
 class MenuButton extends React.Component {
   render() {
     return (
       <button id="MenuButton" onMouseDown={this.props.handleMouseDown}>
-        <img src={Final_R} className="logo_mobile_image" alt="logo" />
+        <RAnimationResized />
       </button>
     )
   }
